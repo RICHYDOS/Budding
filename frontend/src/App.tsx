37f7e8b41af91d7657659c43a5f87ebab9fd174e
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-05 18:45:29 +0100

    Added the product page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import ProductsPage from "./pages/Products";
 import { Routes, Route } from "react-router-dom";
+import ProductPage from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product" element={<ProductPage />} />
       </Routes>
     </div>
   );
