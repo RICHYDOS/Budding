3ea28426ea369ecb2cdeddde08e7e5b39b9f3b31
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-05 18:43:45 +0100

    Created the search input component

--- frontend/src/components/SearchInput/index.tsx
+++ frontend/src/components/SearchInput/index.tsx
@@ -4,12 +4,13 @@ import SearchIcon from "../../Images/SearchInput/search-icon.svg";
 const SearchInput = () => {
   const [searchValue, setSearchValue] = useState("");
   return (
-    <div className="bg-[#f3f3f3] w-full py-[13px] px-[10px] rounded-[18px]">
+    <div className="bg-[#f3f3f3] w-full py-[13px] px-[13px] rounded-[18px] shadow-[0_0_2px_1px_rgba(0,0,0,0.1)]">
       <div className="flex gap-[10px] items-center">
         <figure>
-          <img src={SearchIcon} />
+          <img className="w-[20px]" src={SearchIcon} />
         </figure>
-        <input
+              <input
+                  placeholder="Search Products"
           className="w-full bg-transparent focus:outline-none placeholder:text-[#ccc] text-"
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
