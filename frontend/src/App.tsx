1b1c9158c542f0a1c14479e63d737b06df4b3e39
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-07 18:23:12 +0100

    Added the routes for cart, signup and login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import ProductsPage from "./pages/Products";
 import { Routes, Route } from "react-router-dom";
 import ProductPage from "./pages/Product";
+import CartPage from "./pages/Cart";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/products" element={<ProductsPage />} />
-        <Route path="/product" element={<ProductPage />} />
+        <Route path="/product/:_id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
