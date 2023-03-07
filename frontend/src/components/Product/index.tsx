8cfeaf4985337433bf5cb2ed0d1dd500aaf3b41a
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-07 18:24:39 +0100

    Added props to the product component and made it link to the product page

--- frontend/src/components/Product/index.tsx
+++ frontend/src/components/Product/index.tsx
@@ -1,21 +1,35 @@
-const Product = () => {
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+
+interface ProductProps {
+  _id: string;
+  image: string;
+  shortDescription: string;
+  name: string;
+  price: number;
+}
+
+const Product: React.FC<ProductProps> = ({ image, shortDescription, name, price, _id }) => {
+  const navigate = useNavigate();
   return (
     <div>
-      <div className="w-full object-cover shadow-[0_0_30px_1px_rgba(0,0,0,0.1)] rounded-[14px] overflow-hidden pt-[10px]">
+      <motion.div onClick={() => navigate(`/product/${_id}`)} whileHover={{scale: 1.05}} className="cursor-pointer w-full object-cover shadow-[0_0_30px_1px_rgba(0,0,0,0.1)] rounded-[14px] overflow-hidden pt-[10px]">
         <figure>
           <img
             className="h-[130px] w-full object-cover"
-            src="https://litb-cgis.rightinthebox.com/images/384x384/202209/bps/product/inc/qyzneu1662460508307.jpg?fmt=webp&v=1"
+            src={image}
           />
         </figure>
         <article className="px-[10px] pb-[10px]">
-          <p className="text-[18px] font-bold text-[#3a3a3a] w-full overflow-hidden text-ellipsis whitespace-nowrap">Yummy Yellow</p>
-          <p className="text-[14px] text-[#c8c8ca] leading-[17px] mt-[8px]">
-            Cute protective yellow bear
+          <p className="text-[18px] font-bold text-[#3a3a3a] w-full overflow-hidden text-ellipsis whitespace-nowrap">
+            {name}
           </p>
-          <p className="mt-[8px] text-[19px] font-semibold">$800</p>
+          <p className="text-[14px] text-[#c8c8ca] leading-[17px] mt-[8px]">
+            {shortDescription}
+          </p>
+          <p className="mt-[8px] text-[19px] font-semibold">${price}</p>
         </article>
-      </div>
+      </motion.div>
     </div>
   );
 };
