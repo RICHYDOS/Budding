4f3b98fc380a352d0a0d567403909b66bd13fcf5
Richard Dosunmu <richardosunmu@gmail.com>
2023-03-08 18:03:12 +0100

    Code snippet to create a cart on user registration

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import {User, validate} from "../models/user.js";
 import _ from "lodash";
+import {Cart} from "../models/cart.js";
 import bcrypt from "bcrypt";
 import express from "express";
 const router = express.Router();
@@ -19,7 +20,21 @@ router.post('/', async (req, res) => {
 
     const token = user.generateAuthToken();
 
-    res.header('x-auth-token', token).status(200).send(_.pick(user, ['_id', 'firstname', 'email']));
+    let cart = new Cart({
+        user: user._id
+    });
+    await cart.save();
+
+    const output = {
+        "user": {
+            "_id": user._id,
+            "firstname": user.firstname,
+            "email": user.email
+        },
+        "cart": cart._id
+    }
+
+    res.header('x-auth-token', token).status(200).send(output);
 
 });
 
