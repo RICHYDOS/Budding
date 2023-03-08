eddeb95bafccf99829f5a89ca8b2edf9573c385a
Richard Dosunmu <richardosunmu@gmail.com>
2023-03-08 17:03:54 +0100

    Making some minute changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -32,7 +32,7 @@ export const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id: this._id, email: this.email}, config.get('jwtPrivateKey'));
+    const token = jwt.sign({_id: this._id, email: this.email, firstname: this.firstname}, config.get('jwtPrivateKey'));
     return token;
 }
 
