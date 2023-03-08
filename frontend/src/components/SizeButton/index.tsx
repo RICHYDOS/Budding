d6a12b9393c9c07049ecee4ecd218f6a7d25c04f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-08 11:45:35 +0100

    formatted and made a few changes to the size button

--- frontend/src/components/SizeButton/index.tsx
+++ frontend/src/components/SizeButton/index.tsx
@@ -1,21 +1,30 @@
+import { motion } from "framer-motion";
+
 interface SizeButtonProps {
-    size: string;
-    selected: boolean;
+  size: string;
+  selected: boolean;
+  onClick: () => void;
 }
 
-const SizeButton : React.FC<SizeButtonProps> = ({ size, selected }) => {
-    return (
-        <div
-              style={
-                selected
-                  ? { borderColor: "#fc6e20", backgroundColor: "#fc6e20", color: "white" }
-                  : {}
-              }
-              className="p-[10px] border-2 border-[#c8c8ca] rounded-[10px] font-medium cursor-pointer"
-            >
-              {size}
-            </div>
-    );
-}
+const SizeButton: React.FC<SizeButtonProps> = ({ size, selected, onClick }) => {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      onClick={onClick}
+      style={
+        selected
+          ? {
+              borderColor: "#fc6e20",
+              backgroundColor: "#fc6e20",
+              color: "white",
+            }
+          : {}
+      }
+      className="transiton-all transition-[300ms] p-[10px] border-2 border-[#c8c8ca] rounded-[10px] font-medium cursor-pointer"
+    >
+      {size}
+    </motion.div>
+  );
+};
 
 export default SizeButton;
