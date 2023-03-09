9554dd1e71ad3370575f216a8e1cc8a2e63cdbf6
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-09 15:14:48 +0100

    Added context to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,35 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Loader from "./pages/Loader";
 import { AnimatePresence } from "framer-motion";
+import { QueryClientProvider, QueryClient } from "react-query";
+import ProductContextProvider from "./Context/ProductContext";
+import UserContext from "./Context/UserContext";
+import CartContextProvider from "./Context/CartContext";
+
+const queryClient = new QueryClient();
 
 function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence>
-      <Routes key={location.pathname} location={location}>
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/product/:_id" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Loader />} />
-      </Routes>
-    </AnimatePresence>
+    <QueryClientProvider client={queryClient}>
+      <UserContext>
+        <ProductContextProvider>
+          <CartContextProvider>
+            <AnimatePresence>
+              <Routes key={location.pathname} location={location}>
+                <Route path="/products" element={<ProductsPage />} />
+                <Route path="/product/:_id" element={<ProductPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Loader />} />
+              </Routes>
+            </AnimatePresence>
+          </CartContextProvider>
+        </ProductContextProvider>
+      </UserContext>
+    </QueryClientProvider>
   );
 }
 
